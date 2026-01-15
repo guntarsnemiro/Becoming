e0b3e80f61ca762991471a026998afdd60184081
guntarsnemiro <guntarsnemiro@gmail.com>
2026-01-15 23:35:34 +0200

    Migrate to OpenNext Cloudflare adapter

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+export default defineCloudflareConfig({
+	// For static export landing page, minimal config is needed
+});
